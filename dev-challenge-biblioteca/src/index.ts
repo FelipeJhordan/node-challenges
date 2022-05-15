3ecd175270e315b3a9f2c6343b4163b316f20829
felipejhordan <felipejordan.alves@gmail.com>
2022-05-15 18:53:12 -0300

    feat: update main file with routes and set errorHandle with a custom error handling created before

---
+++ dev-challenge-biblioteca/src/index.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify';
+import { injectDependencies } from './application/ioc';
+import { errorMiddleware } from './infra/http/fastify/middlewares/error-middleware';
+
+const PORT = process.env.PORT || 8080;
+
+const boostrap = async () => {
+  const server = fastify();
+  const bookController = await injectDependencies();
+  server.get('/obras', bookController.find.bind(bookController));
+  server.get('/obras/:id', bookController.findUnique.bind(bookController));
+  server.post('/obras', bookController.create.bind(bookController));
+  server.put('/obras/:id', bookController.update.bind(bookController));
+  server.delete('/obras/:id', bookController.delete.bind(bookController));
+
+  server.setErrorHandler(errorMiddleware);
+
+  server.listen(PORT, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    console.info('Server listening at ' + address);
+  });
+};
+
+boostrap();
