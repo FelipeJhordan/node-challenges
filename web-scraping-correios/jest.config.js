0cb027cbec8b38e1cf2c6ec3da3f9cebcfb201a1
felipejhordan <felipejordan.alves@gmail.com>
2022-05-31 13:07:32 -0300

    chore: add coveragePathIgnorePatterns

--- web-scraping-correios/jest.config.js
+++ web-scraping-correios/jest.config.js
@@ -27,9 +27,11 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: [
+    "\\\\node_modules\\\\",
+    "infra\\**",
+    "shared\\**"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
