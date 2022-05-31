a7294fdb20d2b7ee7281e91aa057dab7d50fa451
felipejhordan <felipejordan.alves@gmail.com>
2022-05-31 13:09:36 -0300

    test: add new test cases

--- web-scraping-correios/test/unit/domain/usecase/location/FindLocationByCEP.usecase.spec.ts
+++ web-scraping-correios/test/unit/domain/usecase/location/FindLocationByCEP.usecase.spec.ts
@@ -1,7 +1,14 @@
 import 'reflect-metadata';
 import { Location } from '../../../../../src/domain/model/location/location.model';
 import { FindLocationByCepUsecase } from '../../../../../src/domain/usecase/location/FindLocationByCEP.usecase';
-const sut = new FindLocationByCepUsecase();
+import { IWebScrapingAdapter } from '../../../../../src/domain/usecase/protocols/adapters/web-scraping/IWebScraping.adapter';
+import { CEP_URL_CONSTANT } from '../../../../../src/shared/consts/cep.constants';
+
+const webscraping: IWebScrapingAdapter = {
+  find: jest.fn(),
+};
+
+const sut = new FindLocationByCepUsecase(webscraping);
 
 const locationStub: Location = {
   city: 'Uberaba',
@@ -21,10 +28,23 @@ describe('<FindLocationByCEP>', () => {
     expect(executeSpy).toBeCalledWith(cep);
   });
   it('Should return the correct Location', async () => {
-    const executeSpy = jest.spyOn(sut, 'execute').mockReturnValueOnce(Promise.resolve(locationStub));
+    jest.spyOn(sut, 'execute').mockReturnValueOnce(Promise.resolve(locationStub));
 
     const Location = await sut.execute(cep);
 
     expect(Location).toEqual(locationStub);
+  });
+
+  it('Should call webscraping with correct params', async () => {
+    const webscrapingSpy = jest.spyOn(webscraping, 'find');
+
+    await sut.execute(cep);
+
+    expect(webscrapingSpy).toBeCalledWith({
+      url: CEP_URL_CONSTANT,
+      body: {
+        cep: cep,
+      },
+    });
   });
 });
--- web-scraping-correios/test/unit/presentation/controllers/location/FindLocationByCEP.controller.spec.ts
+++ web-scraping-correios/test/unit/presentation/controllers/location/FindLocationByCEP.controller.spec.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from 'express';
+import 'reflect-metadata';
 import { FindLocationByCepUsecase } from '../../../../../src/domain/usecase/location/FindLocationByCEP.usecase';
+import { IWebScrapingAdapter } from '../../../../../src/domain/usecase/protocols/adapters/web-scraping/IWebScraping.adapter';
 import { FindLocationByCEPController } from '../../../../../src/presentation/controllers/location/FindLocationByCEP.controller';
-
-const findLocationByCEPUsecase: FindLocationByCepUsecase = {
-  execute: jest.fn(),
+const webscraping: IWebScrapingAdapter = {
+  find: jest.fn(),
 };
+
+const findLocationByCEPUsecase: FindLocationByCepUsecase = new FindLocationByCepUsecase(webscraping);
+findLocationByCEPUsecase.execute = jest.fn();
+
 const sut = new FindLocationByCEPController(findLocationByCEPUsecase);
 
 const requestStub = {
   params: {
     cep: 561378,
+  },
+} as unknown as Request;
+
+const incorrectRequestStub = {
+  params: {
+    cep: 'dsadas',
   },
 } as unknown as Request;
 
@@ -21,12 +32,25 @@ describe('<FindLocationByCep>', () => {
   it('Should receive the correct param', async () => {
     const handleSpy = jest.spyOn(sut, 'handle');
     await sut.handle(requestStub, responseStub);
-    expect(sut.handle).toBeCalledWith(requestStub, responseStub);
+    expect(handleSpy).toBeCalledWith(requestStub, responseStub);
   });
 
   it('Should be return correct values', async () => {
     const responseSpy = jest.spyOn(responseStub, 'json');
     const location = await sut.handle(requestStub, responseStub);
     expect(responseSpy).toBeCalledWith(location);
+  });
+
+  it('Should call the usecase with correct params', async () => {
+    const usecaseSpy = jest.spyOn(findLocationByCEPUsecase, 'execute');
+
+    const location = await sut.handle(requestStub, responseStub);
+
+    expect(usecaseSpy).toBeCalledWith(requestStub.params.cep);
+  });
+
+  it('Should receive incorrect param and throws error', async () => {
+    const response = sut.handle(incorrectRequestStub, responseStub);
+    await expect(response).rejects.toThrow();
   });
 });
