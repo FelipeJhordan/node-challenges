cde5ae03e538cd3ecd8c2ba27ce131a1e1bb856c
felipejhordan <felipejordan.alves@gmail.com>
2022-05-30 21:30:24 -0300

    bugfix: update lint to include jest

--- web-scraping-correios/jest.config.js
+++ web-scraping-correios/jest.config.js
@@ -18,6 +18,7 @@ module.exports = {
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
+  
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
